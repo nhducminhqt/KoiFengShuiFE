8d23daa7a32c2561c1451584ed64f30e717446da
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-11-28 06:59:05 +0700

    change small

--- my-react-app/src/admin/layouts/footerAdmin.tsx
+++ my-react-app/src/admin/layouts/footerAdmin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const FooterAdmin: React.FC = () => {
   return (
@@ -6,7 +6,7 @@ const FooterAdmin: React.FC = () => {
       <div className="bg-light rounded-top p-4">
         <div className="row">
           <div className="col-12 col-sm-6 text-center text-sm-start">
-            &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+            &copy; <a href="#">Your Site Name Here</a>, All Right Reserved.
           </div>
           <div className="col-12 col-sm-6 text-center text-sm-end">
             Designed By <a href="https://htmlcodex.com">HTML Codex</a>
